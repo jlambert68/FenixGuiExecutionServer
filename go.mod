4071407644a8f0e4acb422f155651cc3f9040482
jlamber <jonas.lambert@drakon.se>
2022-09-13 14:34:17 +0200

    2nd commit

--- go.mod
+++ go.mod
@@ -1,3 +1,38 @@
 module FenixGuiExecutionServer
 
 go 1.18
+
+require (
+	github.com/jackc/pgx/v4 v4.17.2
+	github.com/jlambert68/FenixGrpcApi v0.0.0-20220912160640-5d9bbd52e495
+	github.com/jlambert68/FenixSyncShared v0.0.0-20220309201741-aa2c115e0cc1
+	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1
+	google.golang.org/api v0.95.0
+	google.golang.org/grpc v1.49.0
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	cloud.google.com/go/compute v1.7.0 // indirect
+	github.com/go-gota/gota v0.12.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220624142145-8cd45d7dbd1f // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"FenixGuiExecutionServer/common_config"
+	"strconv"
+
+	//"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+// mustGetEnv is a helper function for getting environment variables.
+// Displays a warning if the environment variable is not set.
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.\n", k)
+	}
+	return v
+}
+
+func main() {
+	//time.Sleep(15 * time.Second)
+	fenixGuiExecutionServerMain()
+}
+
+func init() {
+	//executionLocation := flag.String("startupType", "0", "The application should be started with one of the following: LOCALHOST_NODOCKER, LOCALHOST_DOCKER, GCP")
+	//flag.Parse()
+
+	var err error
+
+	// Get Environment variable to tell how this program was started
+	var executionLocation = mustGetenv("ExecutionLocation")
+
+	switch executionLocation {
+	case "LOCALHOST_NODOCKER":
+		common_config.ExecutionLocationForClient = common_config.LocalhostNoDocker
+
+	case "LOCALHOST_DOCKER":
+		common_config.ExecutionLocationForClient = common_config.LocalhostDocker
+
+	case "GCP":
+		common_config.ExecutionLocationForClient = common_config.GCP
+
+	default:
+		fmt.Println("Unknown Execution location for FenixGuiServer: " + executionLocation + ". Expected one of the following: LOCALHOST_NODOCKER, LOCALHOST_DOCKER, GCP")
+		os.Exit(0)
+
+	}
+
+	// Address to GuiBuilderServer
+	common_config.FenixGuiServerAddress = mustGetenv("FenixGuiBuilderServerAddress")
+
+	// Port for GuiBuilderServer
+	common_config.FenixGuiServerPort, err = strconv.Atoi(mustGetenv("FenixGuiBuilderServerPort"))
+	if err != nil {
+		fmt.Println("Couldn't convert environment variable 'FenixGuiBuilderServerPort' to an integer, error: ", err)
+		os.Exit(0)
+
+	}
+
+}
